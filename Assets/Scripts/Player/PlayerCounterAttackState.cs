9f2b814d5a5582bb6b888b8a5e5b0b288221e9a0
zhangchen <zhangchen@partners.poissonsoft.com>
2024-04-15 22:06:44 +0800

    1st commit

---
+++ Assets/Scripts/Player/PlayerCounterAttackState.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class PlayerCounterAttackState : PlayerState
+{
+    private bool canCreateClone;
+    private bool canRecover;
+
+    public PlayerCounterAttackState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        canCreateClone = true;
+        canRecover = true;
+
+        stateTimer = player.counterAttackDuration;
+        player.anim.SetBool("SuccessfulCounterAttack", false);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        player.SetZeroVelocity();
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.attackCheck.position, player.attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                if (hit.GetComponent<Enemy>().CanBeStunned())
+                {
+                    stateTimer = 10; // any value bigger than 1
+                    player.anim.SetBool("SuccessfulCounterAttack", true);
+
+                    player.skill.parry.UseSkill();
+                    if (canRecover)
+                    {
+                        canRecover = false;
+                        player.skill.parry.Recover();
+                    }
+
+                    if (canCreateClone)
+                    {
+                        canCreateClone = false;
+                        player.skill.parry.MakeMirageOnParry(hit.transform);
+                    }
+                }
+            }
+        }
+
+        if (stateTimer < 0 || triggerCalled)
+            stateMachine.ChangeState(player.idleState);
+    }
+}
